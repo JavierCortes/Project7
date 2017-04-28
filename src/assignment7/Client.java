6da4c3a3db0858cd2493aead249279b57efb4270
karthikkonath <karthik.konath@gmail.com>
2017-04-27 20:05:04 -0500

    Created GUI + basic client functionality

--- src/assignment7/Client.java
+++ src/assignment7/Client.java
@@ -1,17 +1,38 @@
 package assignment7;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.*;
 
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class Client {
 	String name;
 	Socket socket;
-	
+	JFrame frame = new JFrame("422C \"It Exists\" Chatroom");
+	private JTextField textField = new JTextField(50);
+	private JTextArea textArea = new JTextArea(10,60);
 	public Client(String n, Socket sock){
+		textArea.setEditable(false);
+		frame.getContentPane().add(textField, BorderLayout.SOUTH);
+		frame.getContentPane().add(new JScrollPane(textArea), BorderLayout.CENTER);
+		frame.pack();
 		name = n;
 		socket = sock;
+		
+		textField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                sendMessage(textField.getText());
+                textField.setText("");
+            }
+        });
 	}
 	
 	public void sendMessage(String m){
@@ -33,5 +54,14 @@ public class Client {
 		}
 		
 		System.out.println(name + " disconnected");
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while(i<1000){
+			i++;
+			textArea.append("\n"+ i);
+		}
 	}
 }
--- src/assignment7/Main.java
+++ src/assignment7/Main.java
@@ -2,6 +2,8 @@ package assignment7;
 
 import java.util.*;
 
+import javax.swing.JFrame;
+
 public class Main {
 	static String ip = "localhost";
 	static int port = 4422;
@@ -9,14 +11,15 @@ public class Main {
 	public static void main(String[] args){
 		ServerMain.main(args);
 		ClientMain clientMain = new ClientMain();
-		
+
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("Enter a name: ");
 		String name = scanner.nextLine();
-		
 		clientMain.connectClient(name, ip, port);
-		
+		clientMain.clients.get(name).frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		clientMain.clients.get(name).frame.setVisible(true);
+		clientMain.clients.get(name).run();
 		if(scanner.nextLine().equals("quit")){
 			clientMain.disconnectClient(name);
 		}
